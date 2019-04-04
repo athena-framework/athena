feae38c6b222f9c91b3a6e0cf9fd31b0438cc802
Blacksmoke16 <yomoejoe@gmail.com>
2019-04-04 19:31:28 -0400

    Make params optional

--- src/commands/generate_config_file_command.cr
+++ src/commands/generate_config_file_command.cr
@@ -4,7 +4,7 @@ struct GenerateConfigFileCommand < Athena::Cli::Command
   self.command_name = "athena:generate:config_file"
   self.description = "Generates the default config file for Athena"
 
-  def self.execute(force : Bool = false, path : String = "./athena.yml") : Nil
+  def self.execute(force : Bool? = false, path : String? = "./athena.yml") : Nil
     # Create the `athena.yml` config file on install if it does not exist.
     pp __DIR__
     if !File.exists?(path) || force
