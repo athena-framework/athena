ac6329039233cdd709d54a720b1746f9960c173a
Blacksmoke16 <yomoejoe@gmail.com>
2019-04-04 19:29:59 -0400

    Ability to specify path

--- src/commands/generate_config_file_command.cr
+++ src/commands/generate_config_file_command.cr
@@ -4,11 +4,11 @@ struct GenerateConfigFileCommand < Athena::Cli::Command
   self.command_name = "athena:generate:config_file"
   self.description = "Generates the default config file for Athena"
 
-  def self.execute(force : Bool? = false) : Nil
+  def self.execute(force : Bool = false, path : String = "./athena.yml") : Nil
     # Create the `athena.yml` config file on install if it does not exist.
     pp __DIR__
-    if !File.exists?("./athena.yml") || force
-      File.open "./athena.yml", "w" do |file|
+    if !File.exists?(path) || force
+      File.open path, "w" do |file|
         file.print "# Config file for Athena.\n"
         file.print Athena::Config::Config.new.to_yaml
       end
