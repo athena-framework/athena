8dbb672a9b571fee75c2f25df7ff5975524e3d0e
George Dietrich <george@dietrich.app>
2020-11-22 18:11:22 -0500

    Fix param fetcher block typing

--- src/params/param_fetcher_interface.cr
+++ src/params/param_fetcher_interface.cr
@@ -1,4 +1,4 @@
 module Athena::Routing::Params::ParamFetcherInterface
   abstract def get(name : String, strict : Bool? = nil)
-  abstract def each(strict : Bool? = nil, &) : Nil
+  abstract def each(strict : Bool? = nil, & : _ -> Nil) : Nil
 end
