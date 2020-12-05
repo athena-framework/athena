9e106f7d44647f5912d2de3dd4bcae60b602b427
George Dietrich <george@dietrich.app>
2020-12-05 14:26:59 -0500

    Use UTF-8 as default charset in default renderers

--- spec/error_renderer_spec.cr
+++ spec/error_renderer_spec.cr
@@ -8,7 +8,7 @@ describe ART::ErrorRenderer do
 
     response = renderer.render exception
 
-    response.headers.should eq HTTP::Headers{"retry-after" => "42", "content-type" => "application/json"}
+    response.headers.should eq HTTP::Headers{"retry-after" => "42", "content-type" => "application/json; charset=UTF-8"}
     response.status.should eq HTTP::Status::TOO_MANY_REQUESTS
     response.content.should eq %({"code":429,"message":"cool your jets"})
   end
@@ -20,7 +20,7 @@ describe ART::ErrorRenderer do
 
     response = renderer.render exception
 
-    response.headers.should eq HTTP::Headers{"content-type" => "application/json"}
+    response.headers.should eq HTTP::Headers{"content-type" => "application/json; charset=UTF-8"}
     response.status.should eq HTTP::Status::INTERNAL_SERVER_ERROR
     response.content.should eq %({"code":500,"message":"Internal Server Error"})
   end
