493278e3a76cb9702eece4d2719300f52f88d04b
Oleh Prypin <oleh@pryp.in>
2021-01-31 23:46:59 +0100

    Simplify URL generation (#101)

--- src/routing/url_generator.cr
+++ src/routing/url_generator.cr
@@ -52,62 +52,37 @@ class Athena::Routing::URLGenerator
     end
 
     # Use any extra passed in params as query params.
-    query = if params && !params.empty?
-              params.compact!
+    query = if params && !(params.compact!).empty?
               HTTP::Params.encode params.transform_values &.to_s.as(String)
             end
 
-    port = nil
-
-    # Only bother setting the port if the `port` value can be extracted from the `host` header, and is no a standard port.
-    if (host_header = @request.headers["host"]?) && (p = host_header.partition(':').last) && !p.in?("", "80", "443")
-      port = p.to_i
-    end
-
-    scheme = "https"
-    base_uri = nil
-
-    # Apply the `base_uri` parameter parts if present.
-    if base_uri = @base_uri.presence
-      base_uri = URI.parse base_uri
-      scheme = base_uri.scheme
-      host = base_uri.host
+    if (b = @base_uri.presence)
+      base_uri = URI.parse(@base_uri) # TODO: cache this
+    elsif (host = @request.hostname)
+      # Only bother setting the port if the `port` value can be extracted from the `host` header, and is no a standard port.
+      if (host_header = @request.headers["host"]?) && (p = host_header.partition(':').last) && !p.in?("", "80", "443")
+        port = p.to_i
+      end
+      base_uri = URI.new(scheme: "https", host: host, port: port)
     else
-      host = @request.hostname
-    end
-
-    # If there is no host and reference_type is aboslute URL,
-    # fallback to absolute path as we wouldn't know what to make the host.
-    if !host.presence && reference_type.absolute_url?
-      host = nil
-      scheme = nil
+      base_uri = URI.new
     end
 
     path = route.path.gsub(/(?:(:\w+))/, merged_params).gsub(/\/+$/, "")
 
-    uri = URI.new(
-      scheme: scheme,
-      host: host,
-      port: port,
-      path: base_uri ? Path.posix(base_uri.request_target).join(path).to_s : path,
-      query: query,
-      fragment: fragment
-    )
-
     case reference_type
     in .absolute_path?
-      String.build do |str|
-        str << uri.request_target
-
-        if f = uri.fragment.presence
-          str << '#' << f
-        end
-      end
-    in .absolute_url?  then uri.to_s
+      base_uri = URI.new(path: base_uri.path)
+    in .absolute_url?
     in .relative_path? then raise NotImplementedError.new("Relative path reference type is currently not supported.")
     in .network_path?
-      uri.scheme = nil
-      uri.to_s
+      base_uri.scheme = nil
     end
+
+    base_uri.tap do |uri|
+      uri.path = Path.posix(uri.path).join(path).to_s
+      uri.query = query
+      uri.fragment = fragment
+    end.to_s
   end
 end
