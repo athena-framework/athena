0d3e362f6899c75630a35a33e19152df343a58eb
George Dietrich <george@dietrich.app>
2021-12-18 16:02:49 -0500

    Correctly handle diff checks

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -2,41 +2,32 @@
 ## Based on https://github.com/laravel/framework/blob/d1ef8e588cc7efc3b3cf925b42f134aa54a4f9c7/bin/split.sh
 
 set -e
-set -x
 
 CURRENT_BRANCH="master"
 
-function remote()
+# Syncs the provided component if changes were made within the provided sub directory
+#
+# $1 - Sub directory path
+# $2 - Component name
+# $3 - Git URL
+function maybeSync()
 {
-    git remote add $1 $2 || true
-    git fetch $1
-}
-
-function split()
-{
+  if ! $(git diff --quiet --exit-code $BEFORE_SHA $AFTER_SHA -- $1); then
+    echo "::group::Syncing $1"
+    git remote add $2 $3 || true
+    git fetch $2
     git subtree push --prefix="$1" $2 $CURRENT_BRANCH
+    echo "::endgroup::"
+  fi
 }
 
-git pull origin $CURRENT_BRANCH
-
-remote config https://github.com/athena-framework/config.git
-remote console https://github.com/athena-framework/console.git
-remote dependency-injection https://github.com/athena-framework/dependency-injection.git
-remote event-dispatcher https://github.com/athena-framework/event-dispatcher.git
-remote framework https://github.com/athena-framework/framework.git
-remote negotiation https://github.com/athena-framework/negotiation.git
-remote serializer https://github.com/athena-framework/serializer.git
-remote spec https://github.com/athena-framework/spec.git
-remote validator https://github.com/athena-framework/validator.git
-remote website https://github.com/athena-framework/website.git
-
-split "src/components/config" config
-split "src/components/console" console
-split "src/components/dependency_injection" dependency-injection
-split "src/components/event_dispatcher" event-dispatcher
-split "src/components/framework" framework
-split "src/components/negotiation" negotiation
-split "src/components/serializer" serializer
-split "src/components/spec" spec
-split "src/components/validator" validator
-split "src/website" website
+maybeSync "src/components/config" config https://github.com/athena-framework/config.git
+maybeSync "src/components/console" console https://github.com/athena-framework/console.git
+maybeSync "src/components/dependency_injection" dependency-injection https://github.com/athena-framework/dependency-injection.git
+maybeSync "src/components/event_dispatcher" event-dispatcher https://github.com/athena-framework/event-dispatcher.git
+maybeSync "src/components/framework" framework https://github.com/athena-framework/framework.git
+maybeSync "src/components/negotiation" negotiation https://github.com/athena-framework/negotiation.git
+maybeSync "src/components/serializer" serializer https://github.com/athena-framework/serializer.git
+maybeSync "src/components/spec" spec https://github.com/athena-framework/spec.git
+maybeSync "src/components/validator" validator https://github.com/athena-framework/validator.git
+maybeSync "src/website" website https://github.com/athena-framework/website.git
