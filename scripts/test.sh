21e6c907003c47479d8ea46b4ec566651813e95d
George Dietrich <george@dietrich.app>
2022-01-07 23:51:34 -0500

    Try this instead

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
-set -e
 
 for component in $(find src/components/ -maxdepth 2 -type f -name shard.yml | xargs -I{} dirname {} | sort); do
   git diff --quiet --exit-code $BASE_SHA $GITHUB_SHA -- $component
   HAS_COMPONENT_CHANGED=$?
   if [[ $GITHUB_EVENT_NAME != 'pull_request' || $HAS_COMPONENT_CHANGED == 1 ]]; then
     echo "::group::$component"
-    crystal spec $component/spec --order random --error-on-warnings --exclude-warnings $component/spec
+    crystal spec $component/spec --order random --error-on-warnings --exclude-warnings $component/spec || exit 1
     echo "::endgroup::"
   fi
 done
