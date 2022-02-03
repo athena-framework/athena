e2ea8f19f3af2c54b7e6bca17d1c62d1320efcfe
George Dietrich <george@dietrich.app>
2022-02-02 22:32:15 -0500

    Run other component specs even if one fails
    Still return an exit code of `1`if one of them fails

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
+EXIT_CODE=0
+
 for component in $(find src/components/ -maxdepth 2 -type f -name shard.yml | xargs -I{} dirname {} | sort); do
   git diff --quiet --exit-code $BASE_SHA $GITHUB_SHA -- $component
   HAS_COMPONENT_CHANGED=$?
   if [[ $GITHUB_EVENT_NAME != 'pull_request' || $HAS_COMPONENT_CHANGED == 1 ]]; then
     echo "::group::$component"
-    crystal spec $component/spec --order random --error-on-warnings --exclude-warnings $component/spec || exit 1
+    crystal spec $component/spec --order random --error-on-warnings --exclude-warnings $component/spec || EXIT_CODE=1
     echo "::endgroup::"
   fi
 done
+
+exit $EXIT_CODE
