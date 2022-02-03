353207cf1e6bbb93838eaad78ab7075631a86c54
George Dietrich <george@dietrich.app>
2022-02-02 22:50:35 -0500

    Add support for `Colorize::Mode` (#149)
    * Will replace `ACON::Formatter::Mode` when Crystal 1.4.0 is released

--- src/components/console/src/formatter/mode.cr
+++ src/components/console/src/formatter/mode.cr
@@ -1,4 +1,13 @@
 # TODO: Remove this type in favor in the stdlib's version when/if https://github.com/crystal-lang/crystal/pull/7690 is merged.
+# TODO: Remove this monkey patch when Crystal 1.4.0 is released, and make it the min supported version
+{% if compare_versions(Crystal::VERSION, "1.4.0-dev") >= 0 %}
+  enum Colorize::Mode
+    def to_sym
+      self
+    end
+  end
+{% end %}
+
 @[Flags]
 enum Athena::Console::Formatter::Mode
   # Makes the text bold.
@@ -19,16 +28,22 @@ enum Athena::Console::Formatter::Mode
   # Makes the text invisible.
   Hidden
 
-  protected def to_sym : Symbol
-    case self
-    when .bold?      then :bold
-    when .dim?       then :dim
-    when .underline? then :underline
-    when .blink?     then :blink
-    when .reverse?   then :reverse
-    when .hidden?    then :hidden
-    else
-      raise ""
+  {% if compare_versions(Crystal::VERSION, "1.4.0-dev") >= 0 %}
+    protected def to_sym : Colorize::Mode
+      Colorize::Mode.parse self.to_s
     end
-  end
+  {% else %}
+    protected def to_sym : Symbol
+      case self
+      when .bold?      then :bold
+      when .dim?       then :dim
+      when .underline? then :underline
+      when .blink?     then :blink
+      when .reverse?   then :reverse
+      when .hidden?    then :hidden
+      else
+        raise ""
+      end
+    end
+  {% end %}
 end
