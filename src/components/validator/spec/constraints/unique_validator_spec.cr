bffb188ff5173e1598de6d1508a9057edaa97d23
George Dietrich <george@dietrich.app>
2022-03-19 14:15:31 -0400

    Ensure only `Indexable` values can be used with `Unique` constraint (#168)

--- src/components/validator/spec/constraints/unique_validator_spec.cr
+++ src/components/validator/spec/constraints/unique_validator_spec.cr
@@ -17,6 +17,12 @@ struct UniqueValidatorTest < AVD::Spec::ConstraintValidatorTestCase
     self.assert_violation "my_message", CONSTRAINT::IS_NOT_UNIQUE_ERROR, value
   end
 
+  def test_invalid_type : Nil
+    expect_raises AVD::Exceptions::UnexpectedValueError, "Expected argument of type 'Indexable', 'Int32' given." do
+      self.validator.validate 123, new_constraint message: "my_message"
+    end
+  end
+
   def valid_values : NamedTuple
     {
       nil:             {nil},
