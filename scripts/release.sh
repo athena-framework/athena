9aaa59647a13990443b8b62be4959f197c0fc88e
George Dietrich <dev@dietrich.pub>
2024-05-09 20:36:14 -0400

    Add `rebuildDocs` helper script (#412)
    * Call it after tagging components

--- scripts/release.sh
+++ scripts/release.sh
@@ -55,10 +55,27 @@ function tag()
   cd $OLDPWD
 }
 
+# Triggers a build of the documentation for the production environment.
+#
+# Requires there be an `ATHENA_GH_TOKEN` ENV var set in order to authenticate with GitHub.
+# The token must be a Fine-grained personal access token with `contents:write` permissions.
+# It must also be approved by an `athena-framework` admin.
+function rebuildDocs()
+{
+  echo curl -LsS \
+    -X POST \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer ${ATHENA_GH_TOKEN:?GH token not set!}" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    https://api.github.com/repos/athena-framework/athena/dispatches \
+    -d '{"event_type":"docs"}'
+}
+
 # Helper script to assist in component release tasks
 #
-# tag     - Creates a signed git tag at the latest commit for the provided component(s). ./scripts/release.sh tag clock:0.1.0 console:0.4.0
-# docPick - Cherry-picks a commit to the docs branch of the provided component. ./scripts/release.sh docPick dotenv abc123
+# tag       - Creates a signed git tag at the latest commit for the provided component(s). ./scripts/release.sh tag clock:0.1.0 console:0.4.0
+# docPick   - Cherry-picks a commit to the docs branch of the provided component. ./scripts/release.sh docPick dotenv abc123
+# buildDocs - Triggers a re-build of https://athenaframework.org/ for the production environment.
 
 METHOD=$1
 
@@ -70,12 +87,19 @@ case $METHOD in
 
       tag $name $version
     done
+
+    # Re-build the docs after tagging every component
+    rebuildDocs
     ;;
   docPick)
     ensureRepoExists $2 docs
 
     git cherry-pick --quiet $3
     git push
+
+    ;;
+  buildDocs)
+    rebuildDocs
 
     ;;
 esac
