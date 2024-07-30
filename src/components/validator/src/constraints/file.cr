250fc5f1712f88cf8a5a8f023b837ee5634902c7
George Dietrich <dev@dietrich.pub>
2024-07-30 10:37:39 -0400

    Fix lint issue

--- src/components/validator/src/constraints/file.cr
+++ src/components/validator/src/constraints/file.cr
@@ -219,6 +219,8 @@ class Athena::Validator::Constraints::File < Athena::Validator::Constraint
 
   class Validator < Athena::Validator::ConstraintValidator
     # :inherit:
+    #
+    # ameba:disable Metrics/CyclomaticComplexity
     def validate(value : _, constraint : AVD::Constraints::File) : Nil
       return if value.nil? || value == ""
 
