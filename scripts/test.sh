df3acbe9c081e66575a2e9c42fe466f1da5202c0
George Dietrich <dev@dietrich.pub>
2024-09-28 22:53:53 -0400

    Scaffold out code coverage reporting implementation

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-DEFAULT_OPTIONS=(-Dstrict_multi_assign -Dpreview_overload_order --order=random --error-on-warnings)
+DEFAULT_BUILD_OPTIONS=(-Dstrict_multi_assign -Dpreview_overload_order --error-on-warnings)
+DEFAULT_OPTIONS=(--order=random)
 CRYSTAL=${CRYSTAL:=crystal}
+WITH_CODE_COVERAGE=${WITH_CODE_COVERAGE:=1}
 
-# Runs the specs for all, or optionally a single component
+# Runs the specs for all, or optionally a single component.
+# Optionally generates code coverage report data as well.
 #
 # $1 - (optional) component name to runs specs for, or "all". Defaults to "all".
 # $2 - (optional) "type" of specs to run: "unit", "compiled", or "all". Defaults to "all".
@@ -23,17 +26,47 @@ then
   exit 1
 fi
 
+# $1 component name
+function runSpecs()
+{
+  $CRYSTAL spec "${DEFAULT_BUILD_OPTIONS[@]}" "${DEFAULT_OPTIONS[@]}" "src/components/$1/spec"
+}
+
+# $1 component name
+function runSpecsWithCoverage()
+{
+  echo "require \"../../src/components/$1/spec/**\"" > "./coverage/bin/$1.cr" && \
+  crystal build "${DEFAULT_BUILD_OPTIONS[@]}" "./coverage/bin/$1.cr" -o "./coverage/bin/$1" && \
+  kcov --clean --cobertura-only --include-path="./src/components/$1/src" "./coverage/$1" "./coverage/bin/$1" "${DEFAULT_OPTIONS[@]}" || EXIT_CODE=1
+}
+
 if [ $COMPONENT != "all" ]
 then
-  $CRYSTAL spec "${DEFAULT_OPTIONS[@]}" "src/components/$1/spec"
+  if [ $WITH_CODE_COVERAGE == "1" ]
+  then
+    runSpecsWithCoverage $COMPONENT
+  else
+    runSpecs $COMPONENT
+  fi
   exit $?
 fi
 
 EXIT_CODE=0
 
-for component in $(find src/components/ -maxdepth 2 -type f -name shard.yml | xargs -I{} dirname {} | sort); do
+# Coverage generation logic based on https://hannes.kaeufler.net/posts/measuring-code-coverage-in-crystal-with-kcov
+mkdir -p coverage/bin
+mkdir -p /tmp/athena/
+
+for component in $(find src/components/ -maxdepth 2 -type f -name shard.yml | xargs -I{} dirname {} | xargs -I{} basename {} | sort); do
   echo "::group::$component"
-  $CRYSTAL spec "${DEFAULT_OPTIONS[@]}" $component/spec || EXIT_CODE=1
+
+  if [ $WITH_CODE_COVERAGE == "1" ]
+  then
+    runSpecsWithCoverage $component || EXIT_CODE=1
+  else
+    runSpecs $component
+  fi
+
   echo "::endgroup::"
 done
 
