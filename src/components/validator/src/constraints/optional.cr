ea4eece9b10380a668d53982baa213d9c9eacdaf
tamdaz <tamda.zohir.pro@gmail.com>
2024-12-13 20:14:31 +0100

    Add a usage for the `@[Assert::Optional]` constraint.

--- src/components/validator/src/constraints/optional.cr
+++ src/components/validator/src/constraints/optional.cr
@@ -1,5 +1,16 @@
 # Allows wrapping `AVD::Constraint`(s) to denote it as being optional within an `AVD::Constraints::Collection`.
 # See [this][Athena::Validator::Constraints::Collection--required-and-optional-constraints] for more information.
+#
+# ```crystal
+# class Post
+#   include AVD::Validatable
+#
+#   def initialize(@content : String); end
+#
+#   @[Assert::Optional]
+#   property content : String
+# end
+# ```
 class Athena::Validator::Constraints::Optional < Athena::Validator::Constraints::Existence
   # :inherit:
   def validated_by : NoReturn
