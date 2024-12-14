e76b861b338f5a88824366de2de691a424603771
tamdaz <tamda.zohir.pro@gmail.com>
2024-12-14 18:59:16 +0100

    Delete examples for the `@[Assert::Required]` and `@[Assert::Optional]` constraints.

--- src/components/validator/src/constraints/optional.cr
+++ src/components/validator/src/constraints/optional.cr
@@ -1,16 +1,5 @@
 # Allows wrapping `AVD::Constraint`(s) to denote it as being optional within an `AVD::Constraints::Collection`.
 # See [this][Athena::Validator::Constraints::Collection--required-and-optional-constraints] for more information.
-#
-# ```
-# class Post
-#   include AVD::Validatable
-#
-#   def initialize(@content : String); end
-#
-#   @[Assert::Optional]
-#   property content : String
-# end
-# ```
 class Athena::Validator::Constraints::Optional < Athena::Validator::Constraints::Existence
   # :inherit:
   def validated_by : NoReturn
