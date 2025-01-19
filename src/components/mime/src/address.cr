bde8ea0f26e842112aaf3b572f7cb05c7298458b
George Dietrich <dev@dietrich.pub>
2025-01-19 11:22:20 -0500

    Start on doc pass

--- src/components/mime/src/address.cr
+++ src/components/mime/src/address.cr
@@ -1,3 +1,4 @@
+# Represents an email address with an optional name.
 struct Athena::MIME::Address
   private FROM_STRING_PATTERN = /(?<displayName>[^<]*)<(?<addrSpec>.*)>[^>]*/
 
@@ -5,19 +6,61 @@ struct Athena::MIME::Address
     AMIME::Encoder::IDNAddress.new
   end
 
+  # Returns the raw email address portion of this Address.
+  # Use `#encoded_address` to get a safe representation for use in a MIME header.
+  #
+  # ```
+  # address = AMIME::Address.new "first.last@example.com", "First Last"
+  # address.address # => "first.last@example.com"
+  # ```
   getter address : String
+
+  # Returns the raw name portion of this Address, or an empty string if none was set.
+  # Use `#encoded_name` to get a safe representation for use in a MIME header.
+  #
+  # ```
+  # address = AMIME::Address.new "first.last@example.com"
+  # address.name # => ""
+  #
+  # address = AMIME::Address.new "first.last@example.com", "First Last"
+  # address.name # => "First Last"
+  # ```
   getter name : String
 
+  # Creates an array of `AMIME::Address` from the provided *addresses*.
+  #
+  # ```
+  # AMIME::Address.create_multiple "me@example.com", "Mr Smith <smith@example.com>", AMIME::Address.new("you@example.com") # =>
+  # # [
+  # #   Athena::MIME::Address(@address="me@example.com", @name=""),
+  # #   Athena::MIME::Address(@address="smith@example.com", @name="Mr Smith"),
+  # #   Athena::MIME::Address(@address="you@example.com", @name=""),
+  # # ]
+  # ```
   def self.create_multiple(*addresses : self | String) : Array(self)
     self.create_multiple addresses
   end
 
+  # Creates an array of `AMIME::Address` from the provided enumerable *addresses*.
+  #
+  # ```
+  # AMIME::Address.create_multiple({"me@example.com", "Mr Smith <smith@example.com>", AMIME::Address.new("you@example.com")}) # =>
+  # # [
+  # #   Athena::MIME::Address(@address="me@example.com", @name=""),
+  # #   Athena::MIME::Address(@address="smith@example.com", @name="Mr Smith"),
+  # #   Athena::MIME::Address(@address="you@example.com", @name=""),
+  # # ]
+  # ```
   def self.create_multiple(addresses : Enumerable(self | String)) : Array(self)
     addresses.map do |a|
       self.create a
     end.to_a
   end
 
+  # Creates a new `AMIME::Address`.
+  #
+  # If the *address* is already an `AMIME::Address`, it is returned as is.
+  # Otherwise if it's a `String`, then attempt to parse the name and address from the provided string.
   def self.create(address : self | String) : self
     return address if address.is_a? self
 
@@ -30,6 +73,7 @@ struct Athena::MIME::Address
     new match["addrSpec"], match["displayName"].strip(" '\"")
   end
 
+  # Creates a new `AMIME::Address` with the provided *address* and optionally *name*.
   def initialize(address : String, name : String = "")
     @address = address.strip
     @name = name.gsub(/\n|\r/, "", options: :no_utf_check).strip
@@ -40,7 +84,11 @@ struct Athena::MIME::Address
   # :nodoc:
   def_clone
 
-  # :nodoc:
+  # Writes an encoded representation of this Address to the provided *io* for use in a MIME header.
+  #
+  # ```
+  # AMIME::Address.new "contact@athenï.org", "George").to_s # => "\"George\" <contact@xn--athen-gta.org>"
+  # ```
   def to_s(io : IO) : Nil
     if name = self.encoded_name.presence
       return io << %("#{name}" <#{self.encoded_address}>)
@@ -49,14 +97,31 @@ struct Athena::MIME::Address
     io << self.encoded_address
   end
 
+  # Returns an encoded representation of `#address` safe to use within a MIME header.
+  #
+  # ```
+  # AMIME::Address.new("contact@athenï.org").encoded_address # => "xn--athen-gta.org"
+  # ```
   def encoded_address : String
     self.class.encoder.encode @address
   end
 
+  # Returns an encoded representation of `#name` safe to use within a MIME header.
+  #
+  # ```
+  # AMIME::Address.new("us@example.com", %(Me, "You)).encoded_name # => "Me, \"You"
+  # ```
   def encoded_name : String
     @name
   end
 
+  # Returns `true` if this Address's localpart contains at least one non-ASCII character.
+  # Otherwise returns `false`.
+  #
+  # ```
+  # AMIME::Address.new("info@dømi.fo").has_unicode_local_part? # => false
+  # AMIME::Address.new("dømi@dømi.fo").has_unicode_local_part? # => true
+  # ```
   def has_unicode_local_part? : Bool
     local, _, _ = @address.partition '@'
 
