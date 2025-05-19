5e3192b420c09d5e132ddc769800dab8fd94825b
George Dietrich <dev@dietrich.pub>
2025-05-19 00:22:25 -0400

    Fix duplicate test case runs with abstract generic parent test case (athena-framework/athena#538)

--- src/components/spec/src/athena-spec.cr
+++ src/components/spec/src/athena-spec.cr
@@ -6,13 +6,14 @@ require "./test_case"
 
 # A set of common [Spec](https://crystal-lang.org/api/Spec.html) compliant testing utilities/types.
 module Athena::Spec
-  VERSION = "0.3.10"
+  VERSION = "0.3.11"
 
   # Runs all `ASPEC::TestCase`s.
   #
   # Is equivalent to manually calling `.run` on each test case.
   def self.run_all : Nil
-    {% for unit_test in ASPEC::TestCase.all_subclasses.reject { |tc| tc.abstract? || tc.annotation(ASPEC::TestCase::Skip) } %}
+    # `#uniq` is to work around https://github.com/crystal-lang/crystal/issues/15793.
+    {% for unit_test in ASPEC::TestCase.all_subclasses.reject { |tc| tc.abstract? || tc.annotation(ASPEC::TestCase::Skip) }.uniq %}
       {{unit_test.id}}.run
     {% end %}
   end
