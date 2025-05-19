632a0eacca14d21548ed1926b607a9948d344328
George Dietrich <dev@dietrich.pub>
2025-05-19 00:12:49 -0400

    Add comment

--- src/components/spec/src/athena-spec.cr
+++ src/components/spec/src/athena-spec.cr
@@ -12,6 +12,7 @@ module Athena::Spec
   #
   # Is equivalent to manually calling `.run` on each test case.
   def self.run_all : Nil
+    # `#uniq` is to work around https://github.com/crystal-lang/crystal/issues/15793.
     {% for unit_test in ASPEC::TestCase.all_subclasses.reject { |tc| tc.abstract? || tc.annotation(ASPEC::TestCase::Skip) }.uniq %}
       {{unit_test.id}}.run
     {% end %}
