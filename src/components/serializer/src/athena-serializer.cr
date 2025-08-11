d47dee4ed11ef8c5224fe61b433eac3a36c0d4fb
George Dietrich <dev@dietrich.pub>
2025-08-10 21:15:12 -0400

    Fix typo

--- src/components/serializer/src/athena-serializer.cr
+++ src/components/serializer/src/athena-serializer.cr
@@ -36,7 +36,7 @@ module YAML; end
 
 # Provides enhanced (de)serialization features.
 module Athena::Serializer
-  VERSION = "0.4.1"
+  VERSION = "0.4.2"
 
   # Returns an `ASR::SerializerInterface` instance for ad-hoc (de)serialization.
   #
